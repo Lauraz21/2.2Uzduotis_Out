d1d23c1269a2a2089dd0f648f7fd03dd2e4acc06
Laura <Laura@DESKTOP-BESSB6E>
2023-09-12 21:27:13 +0300

    Add project files.

---
+++ 2.2Uzduotis_Out/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _2._2Uzduotis_Out
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        bool success = Divide(num1, num2, out double result);
+
+            if (success)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Nepavyko padalinti");
+            }
+        }
+        public static bool Divide(double num1, double num2, out double result)
+        {
+            result = num1 / num2;
+
+            if (num2 == 0)
+            {
+                return false;
+
+            }
+            return true;
+        }
+    }
+}
